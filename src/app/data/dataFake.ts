cfc867166f237fa617253aacefad672faf744855
AlmiranteEdu <eduardoagusmao@hotmail.com>
2024-02-21 02:57:20 -0300

    finishing project

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,20 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"Lorem Ipsum",
+        "description":"Lorem Ipsum",
+        "photoCover":"https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg"
+    },
+    {
+        "id":"2",
+        "title":"Lorem Ipsum",
+        "description":"Lorem Ipsum",
+        "photoCover":"https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg"
+    },
+    {
+        "id":"3",
+        "title":"Lorem Ipsum",
+        "description":"Lorem Ipsum",
+        "photoCover":"https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg"
+    },
+]
